b8387fe20197c5f044dacfd7e5c243b006c735f3
trycrmr <trycrmr@gmail.com>
2020-07-23 13:10:06 -0400

    Inits a todo list with different todos

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-const Todo = () => {
+const Todo = (props) => {
   return (
-    <>
-      <h4>I'm a todo 21.</h4>
-    </>
+    <summary>
+      {props.title}
+      <details>
+        <p>{props.description}</p>
+        Created at {props.createdAt} by {props.createdBy}.
+      </details>
+    </summary>
   );
 };
 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,21 +1,60 @@
 import React from "react";
+import "./TodoList.css";
+import Todo from "./Todo.jsx";
 
-const AddTodoForm = (props) => {
-  return (
-    <ul>
-      <li>A todo</li>
-      <li>A todo</li>
-      <li>A todo</li>
-      <li>A todo</li>
-      <li>A todo</li>
-      <li>A todo</li>
-      <li>A todo</li>
-      <li>A todo</li>
-      <li>A todo</li>
-      <li>A todo</li>
-      <li>A todo</li>
-    </ul>
-  );
+const TodoList = (props) => {
+  const someTodos = [
+    {
+      id: 0,
+      title: "todo title",
+      description: "todo desc",
+      createdAt: "todo created at timestamp",
+      createdBy: "todo created by",
+    },
+    {
+      id: 1,
+      title: "todo title",
+      description: "todo desc",
+      createdAt: "todo created at timestamp",
+      createdBy: "todo created by",
+    },
+    {
+      id: 2,
+      title: "todo title",
+      description: "todo desc",
+      createdAt: "todo created at timestamp",
+      createdBy: "todo created by",
+    },
+    {
+      id: 3,
+      title: "todo title",
+      description: "todo desc",
+      createdAt: "todo created at timestamp",
+      createdBy: "todo created by",
+    },
+    {
+      id: 4,
+      title: "todo title",
+      description: "todo desc",
+      createdAt: "todo created at timestamp",
+      createdBy: "todo created by",
+    },
+  ];
+
+  const processTodos = (arrOfTodos) => {
+    return someTodos.map((thisTodo) => {
+      return (
+        <Todo
+          key={thisTodo.id.toString()}
+          title={thisTodo.title}
+          description={thisTodo.description}
+          createdAt={thisTodo.createdAt}
+          createdBy={thisTodo.createdBy}
+        />
+      );
+    });
+  };
+  return <section>{processTodos(someTodos)}</section>;
 };
 
-export default AddTodoForm;
+export default TodoList;
